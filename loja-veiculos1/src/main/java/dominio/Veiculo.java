3c255c3d7e9801c3a2821989aa3f004aa7362a73
Logon Aluno <logonrmlocal@fiap.com.br>
2022-04-29 09:56:38 -0300

    certo

--- loja-veiculos1/src/main/java/dominio/Veiculo.java
+++ loja-veiculos1/src/main/java/dominio/Veiculo.java
@@ -11,12 +11,15 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "tab_veiculos")
+@Table(name = "tab_veiculo")
 public class Veiculo {
 	
+	//@EmbeddedId	
+	//private VeiculoId codigo;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long codigo;
+	private Long codigo;
 	
 	@Column(length = 60, nullable = false)
 	private String fabricante;
@@ -24,64 +27,83 @@ public class Veiculo {
 	@Column(length = 60, nullable = false)
 	private String modelo;
 	
-	@Column(name ="anoFabricacao", nullable = false)
+	@Column(name = "ano_fabricacao", nullable = false)
 	private Integer anoFabricacao;
 	
-	@Column(name ="anoModelo", nullable = false)
-	private Integer anoModelo;	
+	@Column(name = "ano_modelo", nullable = false)
+	private Integer anoModelo;
 	
 	@Column(precision = 10, scale = 2, nullable = true)
 	private BigDecimal valor;
-	
-	public Veiculo () {
-		
+
+	public Veiculo() {
+
 	}
-	
-	public long getCodigo() {
+
+	public Veiculo(Long codigo, String fabricante, String modelo, Integer anoFabricacao, Integer anoModelo,
+			BigDecimal valor) {
+		super();
+		this.codigo = codigo;
+		this.fabricante = fabricante;
+		this.modelo = modelo;
+		this.anoFabricacao = anoFabricacao;
+		this.anoModelo = anoModelo;
+		this.valor = valor;
+	}
+
+	public Long getCodigo() {
 		return codigo;
 	}
-	public void setCodigo(long codigo) {
+
+	public void setCodigo(Long codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getFabricante() {
 		return fabricante;
 	}
+
 	public void setFabricante(String fabricante) {
 		this.fabricante = fabricante;
 	}
+
 	public String getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+
 	public Integer getAnoFabricacao() {
 		return anoFabricacao;
 	}
+
 	public void setAnoFabricacao(Integer anoFabricacao) {
 		this.anoFabricacao = anoFabricacao;
 	}
+
 	public Integer getAnoModelo() {
 		return anoModelo;
 	}
+
 	public void setAnoModelo(Integer anoModelo) {
 		this.anoModelo = anoModelo;
 	}
+
 	public BigDecimal getValor() {
 		return valor;
 	}
+
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
-	@Override
-	public String toString() {
-		return "Veiculo [codigo=" + codigo + ", fabricante=" + fabricante + ", modelo=" + modelo + ", anoFabricacao="
-				+ anoFabricacao + ", anoModelo=" + anoModelo + ", valor=" + valor + "]";
-	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(anoFabricacao, anoModelo, codigo, fabricante, modelo, valor);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -92,10 +114,14 @@ public class Veiculo {
 			return false;
 		Veiculo other = (Veiculo) obj;
 		return Objects.equals(anoFabricacao, other.anoFabricacao) && Objects.equals(anoModelo, other.anoModelo)
-				&& codigo == other.codigo && Objects.equals(fabricante, other.fabricante)
+				&& Objects.equals(codigo, other.codigo) && Objects.equals(fabricante, other.fabricante)
 				&& Objects.equals(modelo, other.modelo) && Objects.equals(valor, other.valor);
 	}
-	
-	
 
+	@Override
+	public String toString() {
+		return "Veiculo [codigo=" + codigo + ", fabricante=" + fabricante + ", modelo=" + modelo + ", anoFabricacao="
+				+ anoFabricacao + ", anoModelo=" + anoModelo + ", valor=" + valor + "]";
+	}
+	
 }
