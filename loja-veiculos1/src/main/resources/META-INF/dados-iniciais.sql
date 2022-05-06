5b9d338102b8decd03e77173fa4bbb0c3a7576c9
Logon Aluno <logonrmlocal@fiap.com.br>
2022-05-06 10:02:25 -0300

    att loja

--- loja-veiculos1/src/main/resources/META-INF/dados-iniciais.sql
+++
@@ -1,8 +0,0 @@
-insert into tab_veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor)
-values ('Fiat', 'Toro', 2020, 2020, 107000);
-
-insert into tab_veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor)
-values ('Ford', 'Fiesta', 2019, 2019, 42000);
-
-insert into tab_veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor)
-values ('VW', 'Gol', 2019, 2020, 25000);
